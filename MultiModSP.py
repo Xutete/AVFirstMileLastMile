712ce6ab7dbb9153bd8573d07e59f61aa44a8aca
Pramesh Kumar <kumar372@ad.umn.edu>
2019-09-10 17:37:12 -0500

    just added a script for multimodal shortest path

--- MultiModSP.py
+++ MultiModSP.py
@@ -20,7 +20,6 @@ class Node:
         self.lat = float(_tmpIn[0])
         self.long = float(_tmpIn[1])
         self.type = _tmpIn[2]
-        self.nodes = []
         self.outLinks = []
         self.inLinks = []
         self.labels = (999999.0,999999.0) # time, cost
@@ -42,6 +41,31 @@ class Link:
 
 ################################################################################################
 def readNodes():
+    # Reading transit stops
+    inFile = open(inputDataLocation+"ft_input_stops.dat")
+    tmpIn = inFile.readline().strip().split("\t")
+    for x in inFile:
+        tmpIn = x.strip().split("\t")
+        nodeId =  (tmpIn[0], "transit") # A word transit is added because there are a lot of similar roadId and stopId
+        if nodeId not in nodeSet:
+            nodeSet[nodeId] = Node([tmpIn[3], tmpIn[4], "transit"])
+        else:
+            print(tmpIn[0], " stop already present as ", nodeSet[tmpIn[0]].type)
+    inFile.close()
+
+    # Reading road nodes
+    inFile = open(inputDataLocation+"ft_input_roadNodes.dat")
+    tmpIn = inFile.readline().strip().split("\t")
+    for x in inFile:
+        tmpIn = x.strip().split("\t")
+        nodeId =  tmpIn[2]
+        if nodeId not in nodeSet:
+            nodeSet[nodeId] = Node([tmpIn[0], tmpIn[1], "road"])
+        else:
+            print(tmpIn[2], " roadNode already present as ", nodeSet[tmpIn[2]].type)
+    inFile.close()
+    print(len(nodeSet), "nodes in the network")
+
     # Reading zones
     inFile = open(inputDataLocation+"ft_input_zones.dat")
     tmpIn = inFile.readline().strip().split("\t")
@@ -52,36 +76,22 @@ def readNodes():
             nodeSet[zoneId] = Node([tmpIn[1], tmpIn[2], "zone"])
     inFile.close()
 
-    # Reading transit stops
-    inFile = open(inputDataLocation+"ft_input_stops.dat")
-    tmpIn = inFile.readline().strip().split("\t")
-    for x in inFile:
-        tmpIn = x.strip().split("\t")
-        if tmpIn[0] not in nodeSet:
-            nodeSet[tmpIn[0]] = Node([tmpIn[3], tmpIn[4], "transit"])
-    inFile.close()
-
-    # Reading road nodes
-    inFile = open(inputDataLocation+"ft_input_roadNodes.dat")
-    tmpIn = inFile.readline().strip().split("\t")
-    for x in inFile:
-        tmpIn = x.strip().split("\t")
-        if tmpIn[2] not in nodeSet:
-            nodeSet[tmpIn[2]] = Node([tmpIn[0], tmpIn[1], "road"])
-    inFile.close()
-    print(len(nodeSet), "nodes in the network")
-
 def readLinks():
+    # Reading access links
     inFile = open(inputDataLocation+"ft_input_accessLinks.dat")
     tmpIn = inFile.readline().strip().split("\t")
     for x in inFile:
         tmpIn = x.strip().split("\t")
         if (tmpIn[0], tmpIn[1]) not in linkSet:
-            linkSet[tmpIn[0], tmpIn[1]] = Link([tmpIn[0], tmpIn[1], tmpIn[2], tmpIn[3], "access"])
+            linkSet[tmpIn[0], (tmpIn[1], "transit")] = Link([tmpIn[0], tmpIn[1], tmpIn[2], tmpIn[3], "access"])
+            nodeSet[tmpIn[0]].outLinks.append((tmpIn[1], "transit"))
+            nodeSet[(tmpIn[1], "transit")].inLinks.append(tmpIn[0])
         if (tmpIn[1], tmpIn[0]) not in linkSet:
+            nodeSet[(tmpIn[1], "transit")].outLinks.append()
             linkSet[tmpIn[0], tmpIn[1]] = Link([tmpIn[1], tmpIn[0], tmpIn[2], tmpIn[3], "egress"])
     inFile.close()
 
+    # Reading
     print(len(linkSet), "links in the network")
 
 
